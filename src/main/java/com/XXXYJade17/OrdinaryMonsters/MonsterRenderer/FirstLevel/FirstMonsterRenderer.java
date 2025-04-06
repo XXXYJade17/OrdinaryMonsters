d0013ded7a502ab536cf46842bc5831d7cd54937
ycking0504 <17276090504@139.com>
2025-04-06 19:21:05 +0800

    2

--- src/main/java/com/XXXYJade17/OrdinaryMonsters/MonsterRenderer/FirstLevel/FirstMonsterRenderer.java
+++ src/main/java/com/XXXYJade17/OrdinaryMonsters/MonsterRenderer/FirstLevel/FirstMonsterRenderer.java
@@ -17,5 +17,4 @@ public class FirstMonsterRenderer extends MobRenderer<FirstMonster, FirstMonster
     public ResourceLocation getTextureLocation(FirstMonster entity) {
         return TEXTURE;
     }
-
 }
