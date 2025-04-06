aa3ed6a205ddadbcb61eceeff665b6744b18759b
ycking0504 <17276090504@139.com>
2025-04-06 17:32:48 +0800

    Initial commit

---
+++ src/main/java/com/XXXYJade17/OrdinaryMonsters/Monsters/FirstLevel/FirstMonster.java
@@ -0,0 +1,12 @@
+package com.XXXYJade17.OrdinaryMonsters.Monsters.FirstLevel;
+
+import com.XXXYJade17.AttributeCore.Capability.Shackle.Shackle;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.Level;
+
+public class FirstMonster extends Monster {
+    public FirstMonster(EntityType<? extends Monster> entityType, Level level){
+        super(entityType, level);
+    }
+}
